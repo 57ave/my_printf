c53baba50057344d187b1e465269df08deb8b04e
Yanis <yanis.kernoua@epitech.eu>
2023-10-24 10:50:58 +0200

    feat(lib): added lib and header

---
+++ lib/my/my_put_nbr.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2023
+** my_put_nbr.c
+** File description:
+** Put number
+*/
+
+#include "../../include/my.h"
+
+int my_put_nbr(int nb)
+{
+    char *dec_base = "0123456789";
+    char *result = my_putnbr_base(nb, dec_base);
+
+    my_putstr(result);
+    return my_getnbr(result);
+}
