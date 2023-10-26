97137230bad7c1630081a486f1bc37ffca933b7f
Gustave Delecroix <gustave.delecroix@epitech.eu>
2023-10-26 19:41:33 +0200

    feat(verify_flags): add a better error handling on wrong flag, can also now handle multiple argument

--- lib/my/io/printf/my_printf.c
+++ lib/my/io/printf/my_printf.c
@@ -10,12 +10,12 @@
 #include "my_printf.h"
 #include "io/my_io.h"
 
-int format_handling(char const *format, int indice, void **arg_tab)
+int format_handling(char const *format, int indice, void **arg_tab, int i_arg)
 {
     int fmt_tab_size = sizeof(FORMAT_TAB) / sizeof(FORMAT_TAB[0]);
     int size_read = 0;
     conversion_specifier_t conv_spec = {
-        0, NULL, 0, 0, 'd'
+        i_arg, NULL, 0, 0, 'd'
     };
 
     for (int i = 0; i < fmt_tab_size; i++) {
@@ -26,46 +26,25 @@ int format_handling(char const *format, int indice, void **arg_tab)
     return size_read;
 }
 
-int is_conv_specifier(char current_char)
-{
-    static char const CONV_SPECIFIER_TAB[] = {
-        'i', 'd', 'o', 'u', 'x', 'X', 'e', 'E', 'f', 'F', 'g', 'G', 'a', 'A'
-        , 'c', 's', 'p'};
-    int size_cs_tab = sizeof(CONV_SPECIFIER_TAB);
-
-    for (int i = 0; i < size_cs_tab; i++) {
-        if (current_char == CONV_SPECIFIER_TAB[i]) {
-            return 1;
-        }
-    }
-    return 0;
-}
-
-int jump_flags(char const *format, int indice)
-{
-    int nb_of_flags = 1;
-
-    for (; format[indice] != '\0'; indice++) {
-        if (is_conv_specifier(format[indice])) {
-            return nb_of_flags;
-        }
-        nb_of_flags += 1;
-    }
-    return -1;
-}
-
 int call_format_handling(char const *format, void **arg_tab)
 {
     int size_read = 0;
+    int indice_arg = 0;
 
     for (int i_fmt = 0; format[i_fmt] != '\0'; i_fmt++) {
-        if (format[i_fmt] == '%') {
+        if (is_real_flag(format, i_fmt)) {
             i_fmt += jump_flags(format, i_fmt + 1);
-            size_read += format_handling(format, i_fmt, arg_tab);
-        } else {
-            my_putchar(format[i_fmt]);
-            size_read += 1;
+            size_read += format_handling(format, i_fmt, arg_tab, indice_arg);
+            indice_arg++;
+            continue;
         }
+        if (format[i_fmt] == '%' && format[i_fmt + 1] == '%') {
+            my_putchar('%');
+            i_fmt += 2;
+            continue;
+        }
+        my_putchar(format[i_fmt]);
+        size_read += 1;
     }
     return size_read;
 }
