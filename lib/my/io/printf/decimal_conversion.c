d14a1d1f154f2adc00b685a0fc30d72eed399836
Gustave Delecroix <gustave.delecroix@epitech.eu>
2023-10-26 15:03:13 +0200

    style(decimal_conversion): fix a V-3

--- lib/my/io/printf/decimal_conversion.c
+++ lib/my/io/printf/decimal_conversion.c
@@ -10,7 +10,7 @@
 
 int decimal_conversion(conversion_specifier_t *conv_spec, void **arg_tab)
 {
-    int *dec_to_print = (int*) &arg_tab[conv_spec->indice_argument];
+    int *dec_to_print = (int *) &arg_tab[conv_spec->indice_argument];
 
     return my_put_nbr(*dec_to_print);
 }
