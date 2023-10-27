3d6d932b6559193cb0efb2b5ec7d3e65621ec38e
Yanis <yanis.kernoua@epitech.eu>
2023-10-27 14:47:10 +0200

    feat(printf): support for char conversion

---
+++ lib/my/io/printf/char_conversion.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2023
+** my_printf
+** File description:
+** flag_c
+*/
+
+#include "my_printf.h"
+#include "io/my_io.h"
+
+int char_conversion(conversion_specifier_t *conv_spec, void **arg_tab)
+{
+    char *char_to_print = (char *) &arg_tab[conv_spec->indice_argument];
+
+    my_putchar(*char_to_print);
+    return 1;
+}
