f8669e8151552b31d705ddb75763c31dbf3bc829
Yanis <yanis.kernoua@epitech.eu>
2023-10-27 14:53:22 +0200

    style(printf): Major C-F3

--- lib/my/io/printf/char_conversion.c
+++ lib/my/io/printf/char_conversion.c
@@ -10,8 +10,9 @@
 
 int char_conversion(conversion_specifier_t *conv_spec, void **arg_tab)
 {
-    unsigned char *char_to_print = (unsigned char *) &arg_tab[conv_spec->indice_argument];
+    unsigned char *char_to_print;
 
+    char_to_print = (unsigned char *) &arg_tab[conv_spec->indice_argument];
     my_putchar(*char_to_print);
     return 1;
 }
