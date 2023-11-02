9a32fc56f8e46b781cd90d8c873da360c55bf896
Yanis <yanis.kernoua@epitech.eu>
2023-11-02 15:57:34 +0100

    feat(printf): octal support alternate form

--- lib/my/io/printf/octal_conversion.c
+++ lib/my/io/printf/octal_conversion.c
@@ -6,6 +6,7 @@
 */
 
 #include "my_printf.h"
+#include "io/printf/flag_characters/flag_characters.h"
 #include "io/my_io.h"
 
 int octal_conversion(conversion_specifier_t *conv_spec, void **arg_tab)
@@ -13,5 +14,6 @@ int octal_conversion(conversion_specifier_t *conv_spec, void **arg_tab)
     int *octal_to_print = (int *) &arg_tab[conv_spec->indice_argument];
 
     apply_nb_precision(conv_spec, *octal_to_print);
+    alternate_form(conv_spec, arg_tab);
     return my_put_octal(*octal_to_print);
 }
