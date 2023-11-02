562ea4308f749204b51e9ba31e14b4abff47b0fa
yanis <yanis.kernoua@epitech.eu>
2023-11-02 20:39:09 +0100

    feat(printf): alternate form support double

--- lib/my/io/printf/flag_characters/alternate_form.c
+++ lib/my/io/printf/flag_characters/alternate_form.c
@@ -24,11 +24,13 @@ static int is_alternate_needed(char *flag_characters)
     return 0;
 }
 
-int alternate_hexadecimal(conversion_specifier_t *conv_spec)
+static int alternate_hexadecimal(char conv_specifier, unsigned int hexa_nb)
 {
     int char_printed = 0;
-    char conv_specifier = conv_spec->conversion_specifier;
 
+    if (hexa_nb == 0) {
+        return char_printed;
+    }
     if (my_islower(conv_specifier)) {
         my_putstr("0x");
         char_printed += 2;
@@ -39,14 +41,28 @@ int alternate_hexadecimal(conversion_specifier_t *conv_spec)
     return char_printed;
 }
 
-int alternate_octal(unsigned int octal_nb)
+static int alternate_octal(unsigned int octal_nb)
 {
     int char_printed = 0;
 
-    if (octal_nb > 0) {
-        my_putchar('0');
-        char_printed++;
+    if (octal_nb == 0) {
+        return char_printed;
     }
+    my_putchar('0');
+    char_printed++;
+    return char_printed;
+}
+
+static int alternate_double(double x)
+{
+    int char_printed = 0;
+    double frac_part = x - ((long) x);
+
+    if (frac_part != 0) {
+        return char_printed;
+    }
+    my_putchar('.');
+    char_printed++;
     return char_printed;
 }
 
@@ -56,20 +72,20 @@ int alternate_form(conversion_specifier_t *conv_spec, void **arg_tab)
     int *ptr_to_arg = 0;
     char conv_specifier = conv_spec->conversion_specifier;
 
-    if (!is_alternate_needed(conv_spec->flag_characters)) {
-            return char_printed;
-    }
+    if (!is_alternate_needed(conv_spec->flag_characters))
+        return char_printed;
     switch (my_tolower(conv_specifier)) {
         case 'x':
         case 'p':
-            char_printed += alternate_hexadecimal(conv_spec);
+            ptr_to_arg = (int *) &arg_tab[conv_spec->indice_argument];
+            char_printed += alternate_hexadecimal(conv_specifier, *ptr_to_arg);
             break;
         case 'o':
             ptr_to_arg = (int *) &arg_tab[conv_spec->indice_argument];
             char_printed += alternate_octal(*ptr_to_arg);
             break;
         default:
-            alternate_form(conv_spec, arg_tab);
+            alternate_double(*(double *) &arg_tab[conv_spec->indice_argument]);
     }
     return char_printed;
 }
