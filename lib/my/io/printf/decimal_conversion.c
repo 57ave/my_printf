efa9061822b09ccf15de2b4217435c6a760a9db9
Gustave Delecroix <gustave.delecroix@epitech.eu>
2023-11-02 11:59:27 +0100

    feat(number_conversion): add basic precision

--- lib/my/io/printf/decimal_conversion.c
+++ lib/my/io/printf/decimal_conversion.c
@@ -11,6 +11,7 @@
 int decimal_conversion(conversion_specifier_t *conv_spec, void **arg_tab)
 {
     int *dec_to_print = (int *) &arg_tab[conv_spec->indice_argument];
+    int size_precision = apply_nb_precision(conv_spec, *dec_to_print);
 
-    return my_put_nbr(*dec_to_print);
+    return my_put_nbr(*dec_to_print) + size_precision;
 }
--- lib/my/io/printf/hexadecimal_conversion.c
+++ lib/my/io/printf/hexadecimal_conversion.c
@@ -12,5 +12,6 @@ int hexadecimal_conversion(conversion_specifier_t *conv_spec, void **arg_tab)
 {
     int *hex_to_print = (int *) &arg_tab[conv_spec->indice_argument];
 
+    apply_nb_precision(conv_spec, *hex_to_print);
     return my_put_hexadecimal(*hex_to_print, 0);
 }
