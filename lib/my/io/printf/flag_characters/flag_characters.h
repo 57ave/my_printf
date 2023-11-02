ceaa236b9cab5d0e08fa8e7d726b7b8e7821ff53
yanis <yanis.kernoua@epitech.eu>
2023-11-02 20:39:43 +0100

    refactored(printf): changed function scope to static

--- lib/my/io/printf/flag_characters/flag_characters.h
+++ lib/my/io/printf/flag_characters/flag_characters.h
@@ -8,15 +8,9 @@
 #ifndef FLAG_CHARACTERS_H_
     #define FLAG_CHARACTERS_H_
     #include "io/printf/my_printf.h"
-    #include <stddef.h>
-    #include <stdarg.h>
 
 /*Main running functions*/
 
 int alternate_form(conversion_specifier_t *conv_spec, void **arg_tab);
-
-/* Flag characters functions */
-
-int alternate_hexadecimal(conversion_specifier_t *conv_spec);
 
 #endif
