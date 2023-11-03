e0aca7e590036e2b6343463f035c7f2d8d578831
Gustave Delecroix <gustave.delecroix@epitech.eu>
2023-11-03 18:59:55 +0100

    fix(argumetn tab): try to fix a 0 value of double while cast

--- lib/my/io/printf/arguments_tab.c
+++ lib/my/io/printf/arguments_tab.c
@@ -8,7 +8,8 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include "my_printf.h"
-#include <stdio.h>
+#include "strings/my_strings.h"
+
 int count_arguments(char const *format)
 {
     int count_arg = 0;
@@ -25,17 +26,48 @@ int count_arguments(char const *format)
     return count_arg;
 }
 
+#include "stdio.h"
+char *get_type(char const *format, int tab_size)
+{
+    int count_arg = 0;
+    char *type = malloc(sizeof(tab_size));
+
+    for (int i = 0; format[i] != '\0'; i++) {
+        if (is_real_flag(format, i)) {
+            type[count_arg] = format[i  + 1];
+            count_arg++;
+        }
+        if (format[i] == '*' &&
+            (format[i - 1] == '$' || format[i - 1] == '%')) {
+            type[count_arg] = '*';
+            count_arg++;
+        }
+    }
+    type[count_arg] = '\0';
+    printf("type %s\n", type);
+    return type;
+}
 void **get_arg_tab(va_list ap, char const *format)
 {
     int tab_size = count_arguments(format);
+    char *type = get_type(format, tab_size);
     void **arg_tab = malloc(sizeof(void *) * (tab_size + 1));
     int i = 0;
+    double tmp = 0;
 
-    if (arg_tab == NULL) {
+    if (arg_tab == NULL)
         return NULL;
-    }
     for (; i < tab_size; i++) {
-        arg_tab[i] = va_arg(ap, void *);
+        switch (type[i])
+        {
+        case 'f':
+            tmp = va_arg(ap, double);
+            arg_tab[i] = my_memcpy(arg_tab[i], &tmp, sizeof(&tmp));
+            break;
+        default:    
+            arg_tab[i] = va_arg(ap, void *);
+            break;
+        }
     }
     arg_tab[i] = NULL;
     va_end(ap);
